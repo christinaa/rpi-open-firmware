f74f6f92dbb9769788db7e01b7dd24136d864c41
Kristina <tinab@hush.ai>
2016-09-07 07:53:36 +0100

    improve loader and add libfdt to it

--- arm_chainloader/loader.cc
+++ arm_chainloader/loader.cc
@@ -20,47 +20,79 @@ Second stage bootloader.
 #include <drivers/fatfs/ff.h>
 #include <chainloader.h>
 #include <drivers/mailbox.hpp>
+#include <libfdt.h>
+#include <memory_map.h>
 
 #define logf(fmt, ...) printf("[LDR:%s]: " fmt, __FUNCTION__, ##__VA_ARGS__);
 
 FATFS g_BootVolumeFs;
 
 #define ROOT_VOLUME_PREFIX "0:"
+#define KERNEL_LOAD_ADDRESS 0x2000000
 
 extern "C" {
-    void bootLinux(int zero, int machineID, void* dtb, void* kernel);
+	void boot_linux(int zero, int machineID, void* dtb, void* kernel);
 }
+
+static_assert((MEM_USABLE_START+0x800000) < KERNEL_LOAD_ADDRESS,
+	"memory layout would not allow for kernel to be loaded at KERNEL_LOAD_ADDRESS, please check memory_map.h");
 
 struct LoaderImpl {
 	inline bool file_exists(const char* path) {
 		return f_stat(path, NULL) == FR_OK;
 	}
 
-	bool read_file(const char* path, uint8_t** dest, bool shouldAlloc) {
-            /* ensure file exists first */
-            if(!file_exists(path)) return false;
+	bool read_file(const char* path, uint8_t*& dest, size_t& size, bool should_alloc = true) {
+		/* ensure file exists first */
+		if(!file_exists(path))
+			return false;
 
-            /* read entire file into buffer */
-            FIL fp; 
-            f_open(&fp, path, FA_READ);
+		/* read entire file into buffer */
+		FIL fp; 
+		f_open(&fp, path, FA_READ);
 
-            unsigned int len = f_size(&fp);
+		unsigned int len = f_size(&fp);
 
-            if(shouldAlloc) {
-                uint8_t* buffer = (uint8_t*) malloc(len);
-                printf("Buffer (%X) len %d\n", (unsigned int) buffer, len);
-                *dest = buffer;
-            } else {
-                printf("Reading %d to %X\n", len, *dest);
-            }
+		if(should_alloc) {
+			uint8_t* buffer = new uint8_t[len];
+			dest = buffer;
+		}
 
-            f_read(&fp, *dest, len, &len);
-            f_close(&fp);
+		logf("%s: reading %d bytes to 0x%X (allocated=%d) ...\n", path, len, (unsigned int)dest, should_alloc);
 
-            return true;
+		size = len;
+
+		f_read(&fp, dest, len, &len);
+		f_close(&fp);
+
+		return true;
 	}
 
-	LoaderImpl() {
+	inline bool read_file(const char* path, uint8_t*& dest, bool should_alloc = true) {
+		size_t size;
+		return read_file(path, dest, size, should_alloc);
+	}
+
+	void* load_fdt(const char* filename, uint8_t* cmdline) {
+		/* read device tree blob */
+		uint8_t* fdt = reinterpret_cast<uint8_t*>(MEM_USABLE_START);
+		size_t sz;
+
+		if(!read_file(filename, fdt, sz, false)) {
+			panic("error reading fdt");
+		}
+
+		int res = fdt_check_header(reinterpret_cast<void*>(fdt));
+		if (res != 0) {
+			panic("fdt blob invalid, fdt_check_header returned %d", res);
+		}
+
+		logf("valid fdt loaded at 0x%X\n", (unsigned int)fdt);
+
+		return reinterpret_cast<void*>(fdt);
+	}
+
+	LoaderImpl() {	
 		logf("Mounting boot partitiion ...\n");
 		FRESULT r = f_mount(&g_BootVolumeFs, ROOT_VOLUME_PREFIX, 1);
 		if (r != FR_OK) {
@@ -68,39 +100,40 @@ struct LoaderImpl {
 		}
 		logf("Boot partition mounted!\n");
 
-                /* dump cmdline.txt for test */
-                uint8_t* arguments;
+		size_t sz;
 
-                if(!read_file("cmdline.txt", &arguments, true)) {
-                    panic("Error reading cmdline arguments");
-                }
+		/* dump cmdline.txt for test */
+		uint8_t* cmdline;
 
-                printf("\n%s\n", arguments);
+		if(!read_file("cmdline.txt", cmdline, sz)) {
+			panic("error reading cmdline");
+		}
 
-                free(arguments);
+		/* nul terminate it */
+		cmdline[sz - 1] = 0;
+		logf("kernel cmdline: %s\n", cmdline);
+		
+		/* load flat device tree */
+		void* fdt = load_fdt("rpi.dtb", cmdline);
+		if (!fdt) {
+			panic("fdt pointer is null");
+		}
 
-                /* read device tree blob */
-                uint8_t* dtb;
+		/* after load_fdt is done with it, we don't need it anymore */
+		delete[] cmdline;
 
-                if(!read_file("rpi.dtb", &dtb, true)) {
-                    panic("Error reading device tree blob");
-                }
+		/* read the kernel -- necessarily at fixed address */
+		uint8_t* zImage = reinterpret_cast<uint8_t*>(KERNEL_LOAD_ADDRESS);
 
-                printf("DTB loaded at %X\n", (unsigned int) dtb);
+		if(!read_file("zImage", zImage, false)) {
+			panic("error reading zImage");
+		}
 
-                /* read the kernel -- necessarily at fixed address */
-                uint8_t* zImage = (uint8_t*) 0x8000;
-
-                if(!read_file("zImage", &zImage, false)) {
-                    panic("Error reading zImage");
-                }
-
-                printf("bzImage loaded at %X\n", (unsigned int) zImage);
-
-                printf("Jumping to the Linux kernel...\n");
-                
-                /* this should never return */
-                bootLinux(0, 3139, dtb, zImage);
+		logf("zImage loaded at 0x%X\n", (unsigned int)zImage);
+		logf("Jumping to the Linux kernel...\n");
+		
+		/* this should never return */
+		boot_linux(0, ~0, fdt, zImage);
 	}
 };
 
