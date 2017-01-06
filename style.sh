32eb9a9294805bc1987de73556a297e461663a99
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2017-01-05 22:55:31 -0800

    Add astyle script for the project

---
+++ style.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# runs astyle on our code to conform to project standards
+# contributors should run this prior to PRs
+
+# a handful of subprojects (BCM headers, libfdt, etc.) are included in-tree
+# to avoid create merge conflicts when pulling upstream, only style our code
+FILES="*.c *.h arm_chainloader/*.c arm_chainloader/*.cc arm_chainloader/*.h arm_chainloader/drivers/*.cc arm_chainloader/drivers/*.hpp"
+
+# run astyle with tabs and attached braces
+astyle --style=attach --indent=tab $FILES
